2147d480823883ff9737e3a23de78343698a7dbd
dasomyun <dsy0302@gmail.com>
2018-08-25 19:18:29 +0900

    Javascript review - Comparison

---
+++ syntax/Comparison.js
@@ -0,0 +1,6 @@
+console.log(1==1);  // true
+console.log(1==2);  // false
+console.log(1>2);   // false
+console.log(1<2);   // true
+console.log(1===1);  // true 이유가 없으면 ==보다 이걸 쓰기
+console.log(1===2);  // false
