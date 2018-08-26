05d5f89ea26f8db3d9b9719ccc41adada9eb82bf
dasomyun <dsy0302@gmail.com>
2018-08-26 16:38:25 +0900

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return  `
     <!doctype html>
     <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
-      <a href="/create">create</a>
+      ${control}
       ${body}
     </body>
     </html>
@@ -43,7 +43,7 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = templateList(fileList);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
           response.writeHead(200);  // 200 성공적으로 파일 열림
           response.end(template);
         });
@@ -52,7 +52,7 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){ //template literal 주의
           var title = queryData.id;
           var list = templateList(fileList);
-          var template =  templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template =  templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);  // 200 성공적으로 파일 열림
           response.end(template);
         });
@@ -63,7 +63,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(fileList);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -72,8 +72,7 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-
-          `);
+          `,'');
         response.writeHead(200);  // 200 성공적으로 파일 열림
         response.end(template);
       });
@@ -91,7 +90,29 @@ var app = http.createServer(function(request,response){
           response.end();
         });
       });
-    } else {
+    } else if(pathname === '/update') {
+      fs.readdir('./data', function(error, fileList) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){ //template literal 주의
+        var title = queryData.id;
+        var list = templateList(fileList);
+        var template =  templateHTML(title, list,
+          `
+          <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+        response.writeHead(200);  // 200 성공적으로 파일 열림
+        response.end(template);
+      });
+    });
+  } else {
       response.writeHead(404);  // 에러
       response.end('Not found');
     }
